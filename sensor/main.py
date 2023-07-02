dd8506bb88f74b54527de50a162e2dcfc2ecbaf6
TheLuckyLuke1337 <112425988+TheLuckyLuke1337@users.noreply.github.com>
2023-07-02 16:29:43 +0200

    Some tweeks and the dht sensor.
    I added the dht sensor and tweeked the output from the read_soil_sensor function.

--- sensor/main.py
+++ sensor/main.py
@@ -1,24 +1,34 @@
+import dht
 from machine import Pin, ADC
 from time import sleep_ms
 
+from sensor_utils import read_soil_sensor, read_dht_sensor
 
-from sensor_utils import read_soil_sensor, analog2percent
+PERIOD_TIME = 2000    # PERIOD_TIME must be larger than
+PUMPING_TIME = 1000   # PUMPING_TIME
+MOISTURE_THRESHOLD = 50
 
-led = Pin(5, Pin.OUT)
 
-water_level = Pin(0, Pin.IN)
-soil_moisture = ADC(0)
+dht_sensor = dht.DHT11(Pin(27))
+
+water_sensor = Pin(0, Pin.IN)
+water_pump = Pin(1, Pin.OUT)
+
+soil_moisture_sensor = ADC(0)
 soil_moisture_power = Pin(28, Pin.OUT)
 
-led.value(0)
-
-
-for _ in range(100):
-    sensor_read = water_level.value()
-    led.value(sensor_read)
-    # print(f"Sensor value = {sensor_read}")
-    soil_read = read_soil_sensor(soil_moisture, soil_moisture_power)
-    soil_moisture_power.value(1)
-    print(analog2percent(soil_read), soil_read)
-    soil_moisture_power.value(0)
-    sleep_ms(200-10)
+while True:
+    read_dht_sensor(dht_sensor)
+    temperature = dht_sensor.temperature()
+    humidity = dht_sensor.humidity()
+    water_level = water_sensor.value()
+    soil_moisture = read_soil_sensor(soil_moisture_sensor, soil_moisture_power)
+    print(f"Soil: {soil_moisture:.0f}% Water: {water_level}\n" +
+          f"Temperature: {temperature} C Humidity: {humidity}%")
+    if water_level and soil_moisture < MOISTURE_THRESHOLD:
+        water_pump.value(1)
+        sleep_ms(PUMPING_TIME)
+        water_pump.value(0)
+        sleep_ms(PERIOD_TIME-PUMPING_TIME)
+    else:
+        sleep_ms(PERIOD_TIME)
--- sensor/sensor_utils.py
+++ sensor/sensor_utils.py
@@ -1,6 +1,13 @@
 from time import sleep_ms
 
 
+def read_dht_sensor(dht_sensor):
+    try:
+        dht_sensor.measure()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def read_soil_sensor(soil_moisture, soil_moisture_power=None):
     """ Activates the sensor for 10 ms then reads the adc """
     # 16200 ~ 100%, 53450 ~ 0%
@@ -13,9 +20,8 @@ def read_soil_sensor(soil_moisture, soil_moisture_power=None):
         soil_moisture_power.value(0)
     else:
         reading = soil_moisture.read_u16()
+    return analog2percent(reading)
 
-    return reading
 
-
-def analog2percent(x, in_min=53450, in_max=16200, out_min=0, out_max=100):
+def analog2percent(x, in_min=53150, in_max=20200, out_min=0, out_max=100):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
