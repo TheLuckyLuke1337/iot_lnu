5e1279898f5627d7fe1805401b3531fdbc96f36a
TheLuckyLuke1337 <112425988+TheLuckyLuke1337@users.noreply.github.com>
2023-07-07 09:37:07 +0200

    Update sensor_packet.py
    Some helpfull functions for tracking what packets have been sent.

--- sensor_packet.py
+++ sensor_packet.py
@@ -2,11 +2,22 @@ from attrs import define
 from typing import Any
 
 
-@define(frozen=True)
+@define
 class SensorPacket:
 
     sensor_type: str
     sensor_value: Any
+    transmitted: bool = False
+
+    def get_data(self):
+        return self.sensor_type, self.sensor_value
+
+    def has_been_transmitted(self):
+        self.transmitted = True
+
+    def get_data_for_transmition(self):
+        self.has_been_transmitted()
+        return self.get_data()
 
 
 def main():
